6484e743e98eec668b0f5bf732f9c0cffbd3cd7b
Jason Raskin <jasonraskin@mac.com>
2025-05-02 14:59:14 -0500

    Fixed up

--- node_helper.js
+++ node_helper.js
@@ -1,9 +1,6 @@
-console.log("[Helper] attempting to read standings file at:", path.join(__dirname, "standings.json"));
-
 const NodeHelper = require("node_helper");
 const moment     = require("moment");
 const fs         = require("fs");
-const path       = require("path");
 
 console.log("[MMM-MLBScoresAndStandings] helper started");
 
@@ -37,13 +34,11 @@ module.exports = NodeHelper.create({
     const date = moment().format("YYYY-MM-DD");
     const url  = `https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=${date}&hydrate=linescore`;
     try {
+      console.log("[MMM] fetching schedule URL:", url);
       const res  = await fetch(url);
       const json = await res.json();
       this.games = json.dates?.[0]?.games || [];
-      console.log(
-        "[MMM-MLBScoresAndStandings] fetched games:",
-        this.games.length
-      );
+      console.log("[MMM-MLBScoresAndStandings] fetched games:", this.games.length);
       this.sendSocketNotification("GAMES", this.games);
     } catch (e) {
       console.error("[MMM-MLBScoresAndStandings] fetchData error", e);
@@ -52,20 +47,15 @@ module.exports = NodeHelper.create({
   },
 
   fetchStandingsFromFile() {
-    const filePath = path.join(__dirname, "standings.json");
+    const filePath = __dirname + "/standings.json";
+    console.log("[MMM] reading standings from file:", filePath);
     try {
       const data    = fs.readFileSync(filePath, "utf8");
       const records = JSON.parse(data);
-      console.log(
-        "[MMM-MLBScoresAndStandings] loaded standings from file:",
-        records.length
-      );
+      console.log("[MMM-MLBScoresAndStandings] loaded standings from file:", records.length);
       this.sendSocketNotification("STANDINGS", records);
     } catch (e) {
-      console.error(
-        "[MMM-MLBScoresAndStandings] read standings file error",
-        e
-      );
+      console.error("[MMM-MLBScoresAndStandings] read standings file error", e);
       this.sendSocketNotification("STANDINGS", []);
     }
   }
